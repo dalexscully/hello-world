ba954d5cfdbf6c31453ce2107e8d25cc34ada710
Domaine Scully <dalexscully@gmail.com>
2022-09-01 15:22:42 -0400

    Completed lab 4

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let myName;
+console.log(myName);
+const myName = 'Domaine'
+console.Log('hello world');
+
+//declare a variable called x assign it valve of the value 10
+let x = 10;
+console.log(x);
