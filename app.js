7c60bcbf6f5b0cc122a36eb553ba23529260eddb
Domaine Scully <dalexscully@gmail.com>
2022-09-09 03:19:43 -0400

    File updated

--- app.js
+++ app.js
@@ -1,75 +1,30 @@
 'use strict';
 
 // proof of life --> to make sure it's connected to my HTML
-// console.log('Hello World!');
 
-// What/why functions?
-// Command we can use to execute a set of steps
+function buyer(){
+    let buyer = prompt('What is your name?');
+    document.write('Hi ' + buyer);
+    
+    let borrower = prompt('Mortgage loan limit? (100000-200000)');
+    let message = '';
+    
+    let client = prompt('Try different loan limit');
 
-// Structure of a function
-// keyword function name
-// functions myfunction(parameters){code to execute}
-//                   parameters code to execute
 
-// function greetUser(){
-//   let usersName = prompt('What is your name?');
-//   document.write('Hi ' + usersName);
-// }
-// greetUser();
-
-// function greetUser(){
-//     let usersName = prompt('what is your name');
-//     document.write('Hi ' + usersName);
-//     return usersName;
-//   }
-  
-// let usersName = greetUser();
-// //   console.log(usersName);
-
-// function customMessage(name){
-// let message;
-
-// if (usersName == 'Nadia'){
-//   message = ' Hi Teach!'  
-// } else if (usersName == 'Bill') {
-//   message = ' Hi Bill!';
-// } else if (usersName == 'Lelia'){
-//   message = ' Hi Lelia!';
-// } else {
-//   message = ' Welcome to my site!'; 
-// } 
-// document.write(message);
-// }
-
-// customMessage('usersName');
-
-//  let message;
-
-                        // parameters
-// function addTwoNumbers(num1, num2){
-//  return num1 + num2
+    if(buyer => 150000){
+        message = 'loan is pending!';
+    } else if (buyer => 120000){
+        message = 'loan is being consider';
+    } else if (buyer < 100000){
+        message = 'You loan is under review!';
+    } else {
+        message = 'That loan amount is not attainable!'
+    }
+    return message;
 }
-// console.log(addTwoNumbers(462, 9));
-                        //  arguments   
-// let myNewNumber = addTwoNumbers(10, 10);
+buyer();
+    
+    
 
 
-// let usersGuess;
-// let guessesLeft = 3;
-
-// while (usersGuess !== 'good' && guessesLeft > 0){
-//     guessesLeft--;
-//     usersGuess = prompt('What is the opposite of bad? (Hint: starts with a g)')
-// }
-// function showhome2(){
-//     let usersNum = prompt('How many homes would you rate my page? 1-5')
-
-// for (let i = 0; ((i < usersNum) && i < 5); i++){
-//     document.write('<img src="home2.jpeg" alt="home2" class="home2"/>');
-//     }
-// }
-
-// showhome2();
-
-// for-loop
-// for-loop (initial value ; condition to evaluate ; increment {code to execute})
