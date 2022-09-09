a16bb5ebf0b2322e6ba1f0b78f4edda24cac64ac
Domaine Scully <dalexscully@gmail.com>
2022-09-09 12:35:01 -0400

    File updated

--- app.js
+++ app.js
@@ -2,24 +2,25 @@
 
 // proof of life --> to make sure it's connected to my HTML
 
+
 function buyer(){
     let buyer = prompt('What is your name?');
-    document.write('Hi ' + buyer);
+    document.write('Hi ' + buyer + ' Let us get started on your loan');
     
-    let borrower = prompt('Mortgage loan limit? (100000-200000)');
+    let borrower = prompt('Mortgage loan limit? ($100000-$200000)');
     let message = '';
     
     let client = prompt('Try different loan limit');
 
 
-    if(buyer => 150000){
+    if(buyer => '$150000'){
         message = 'loan is pending!';
-    } else if (buyer => 120000){
+    } else if (buyer => '$120000'){
         message = 'loan is being consider';
-    } else if (buyer < 100000){
+    } else if (buyer < '$100000'){
         message = 'You loan is under review!';
     } else {
-        message = 'That loan amount is not attainable!'
+        message = 'Let us help you buy your dream home!'
     }
     return message;
 }
